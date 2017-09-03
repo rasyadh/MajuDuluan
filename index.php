94ecc794c29bff206456ee04716e3f1ce4de2ddc
RASYADH\Rasyadh A Aziz <rasyadhabdulaziz@gmail.com>
2017-09-03 13:59:40 +0700

    test profile

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ $app->get('/profile', function($req, $res) use ($bot){
     $result = $bot->getProfile($userId);
 
     $profile = json_decode($result, true);
-    $print_r($profile);
+    print_r($profile);
 
     // return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
 });
